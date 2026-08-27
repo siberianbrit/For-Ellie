6073602bfb9d0c529a919fe64fb21c4ea25e2b03
siberianbrit <142810223+siberianbrit@users.noreply.github.com>
2026-08-27 23:40:01 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE="ellie-v3"; self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","index.html","manifest.json","icon.png"])))); self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
